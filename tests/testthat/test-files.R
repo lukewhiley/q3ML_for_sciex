e9cc7799fc401f7d8d1b3e2d00de592bb3a037ff
wilsontom <wilsontom89@gmail.com>
2022-02-16 22:27:32 +0000

    move function out

--- tests/testthat/test-files.R
+++ tests/testthat/test-files.R
@@ -2,16 +2,17 @@
 test_that('file versions', {
 
 
-  testfiles <-
+  testfile <-
     list.files(system.file('extdata', package = 'q3ML'), pattern = '.mzML', full.names = TRUE)
 
-  expect_null(openFile(testfiles[1]))
-  expect_true(is.list(openFile(testfiles[2])))
+  q3ML_pwiz3_2 <- openFile(testfile)
+
+  expect_true(is.list(q3ML_pwiz3_2))
 
   mzR_pwiz3_1_hd <- readRDS(system.file('extdata/mzR_hdr.rds', package = 'q3ML'))
   mzR_pwiz3_1_pks <- readRDS(system.file('extdata/mzR_pks.rds', package = 'q3ML'))
 
-  q3ML_pwiz3_2 <- openFile(testfiles[2])
+  q3ML_pwiz3_2 <- openFile(testfile)
 
   expect_identical(mzR_pwiz3_1_pks, q3ML_pwiz3_2$peaks)
   expect_identical(mzR_pwiz3_1_hd, q3ML_pwiz3_2$header)
